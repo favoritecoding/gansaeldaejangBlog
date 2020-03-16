319d33b59f976cbd4f23b73e38830dd3a660a184
sangago <sangago.dev@gmail.com>
2020-03-16 16:15:42 +0900

    이미지 확대 테스트 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 // turn img alt into caption
-$('.post-content > p > img[alt]').replaceWith(function () {
+$('img[alt]').replaceWith(function () {
     return '<figure>'
         + '<a href="' + $(this).attr('src') + '" class="mg-link">'
         + '<img src="' + $(this).attr('src') + '"/></a>'
